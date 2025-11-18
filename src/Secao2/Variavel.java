7543390840b3c5301ecf72e0b1354eb8bfd67b81
Paulo Gabriel Afonso <pafonso@makeone.com.br>
2025-11-18 04:29:22 -0300

    mapeando arquivos não mapeados

---
+++ src/Secao2/Variavel.java
@@ -0,0 +1,11 @@
+package Secao2;
+
+public class Variavel {
+    public static void main(String[] args) {
+        //1 - O que são variaveis
+        // tipo -> nome -> atribuir um valor;
+        String nome = "Paulo";
+
+        System.out.println(nome);
+    }
+}

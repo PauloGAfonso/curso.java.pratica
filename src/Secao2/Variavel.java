8ba0a506a34dadaa352673548973f6d4b65683a9
Paulo Gabriel Afonso <pafonso@makeone.com.br>
2025-11-25 04:15:10 -0300

    definindo variáveis

--- src/Secao2/Variavel.java
+++ src/Secao2/Variavel.java
@@ -5,7 +5,23 @@ public class Variavel {
         //1 - O que são variaveis
         // tipo -> nome -> atribuir um valor;
         String nome = "Paulo";
-
         System.out.println(nome);
+
+        int idade = 26;
+        System.out.println(idade);
+
+        //Tipos de dados
+        Boolean CondicionaisLogicas = false;
+        byte ManipulacaoBinaria = 0;
+        int numerosInteiros = 0;
+        long inteirosGrandes = 0;
+        float flutuanteOuFracionais = 1.5f;
+        double flutantesOuFracionaisMaiores = 1.5d;
+        char umaUnicaLetra = 'a';
+        String testando = "Hello, World";
+        String outroTeste = testando;
+
+        System.out.println(outroTeste); //Devido a vc ter associado a variável 'testando' a variável 'outroTeste', tudo que estiver na variável testando mudará na variável outroeste
+        
     }
 }

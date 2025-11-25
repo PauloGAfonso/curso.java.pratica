8a4aec00ea459aa638ef969798bb2dc4f946056e
Paulo Gabriel Afonso <pafonso@makeone.com.br>
2025-11-25 05:07:07 -0300

    Avançando nos estudos

--- src/Secao2/Variavel.java
+++ src/Secao2/Variavel.java
@@ -17,11 +17,49 @@ public class Variavel {
         long inteirosGrandes = 0;
         float flutuanteOuFracionais = 1.5f;
         double flutantesOuFracionaisMaiores = 1.5d;
-        char umaUnicaLetra = 'a';
-        String testando = "Hello, World";
-        String outroTeste = testando;
-
-        System.out.println(outroTeste); //Devido a vc ter associado a variável 'testando' a variável 'outroTeste', tudo que estiver na variável testando mudará na variável outroeste
+        char umaUnicaLetra = 'a'; // -> utilizando aspas únicas é char.
         
+        
+        // Atribuição de variável com outra
+        String testando = "Hello, World"; //-> utilizando aspas duplas é string.
+        String outroTeste = testando;
+        
+        
+        /*
+        Devido a vc ter associado a variável 'testando' a variável 'outroTeste', tudo que estiver na variável testando mudará na variável outroeste
+        */
+        System.out.println(outroTeste); 
+
+
+        // Comentários de uma linha
+
+        /*
+            Comentário usado para explicação mais longas
+         */
+
+
+        /**
+         * Comentários
+         * para
+         * documentação
+         */
+
+            // testando - Ctrl + K, Ctrl + C vc transforma a linha ou linhas selecionada(s) em comentário no vscode.
+
+
+        // - STRINGS
+
+        String firstName = "Paulo";
+        String LastName = "Afonso";
+
+        // System.out.println(firstName + " " + LastName);
+
+        String fullName = firstName + " " + LastName;
+
+        System.out.println(fullName);
+
+
+
+
     }
 }

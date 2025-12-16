ab842c8edd1ba5df71096859e8e2394a5531ea89
Paulo Gabriel Afonso <pafonso@makeone.com.br>
2025-12-16 05:52:50 -0300

    Iniciando na pratica condicionais em Java

---
+++ src/Secao4/Booleanos.java
@@ -0,0 +1,16 @@
+package Secao4;
+
+public class Booleanos {
+    public static void main(String[] args) {
+        // boolean isTrue = true;
+        // boolean isFalse = false;
+
+        // System.out.println(isTrue);
+        // System.out.println(isFalse);
+
+        int x = 10;
+        System.out.println(x == 10);
+        System.out.println(10 != 10);
+    }
+
+}

4af4452b555e14f5439ab45cdb3b36efba927fff
Paulo Gabriel Afonso <pafonso@makeone.com.br>
2025-12-25 05:16:46 -0300

    atualizando

---
+++ src/Secao4/AtribuicaoeComparacao.java
@@ -0,0 +1,17 @@
+package Secao4;
+
+public class AtribuicaoeComparacao {
+    public static void main(String[] args) {
+        //Atribuicao
+        System.out.println("Atribuindo... ");
+        int n = 5;
+        System.out.println("O valor atribuido é " + n);
+
+        // comparacao    
+        System.out.println("Comparando");
+        System.out.println(n == 5);
+    }
+    
+    
+
+}
